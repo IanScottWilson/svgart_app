f4550a84054ad72da75c41e6589e4080a3d1b466
IanScottWilson <ianscottwilson@yahoo.co.uk>
2012-10-11 15:36:07 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SvgartApp::Application.config.secret_token = '724d37a8bb0ed9084e45207dd03eb6654442923e2ca78495ca921a4b629344460e5dc38e1ec2ed593e2d1085eeaa9490d422911ffdd210eaed8cb117ef00c232'
